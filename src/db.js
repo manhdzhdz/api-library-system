ae5d8167c67df544d05e8640bc3a9a67a22329f2
manhdzcrack <147672487+manhdzcrack@users.noreply.github.com>
2023-10-18 18:45:54 -0600

    modificación db

--- src/db.js
+++ src/db.js
@@ -1,9 +1,19 @@
-import { createConnection, createPool } from "mysql2/promise";
-//permite la conexion a la BD
-export const pool = createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    port:"3306",
-    database:"library"
+import { createPool } from "mysql2/promise";
+
+// createPool es equivalente al connection 
+import {
+    DB_HOST,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER,
+    DB_DATABASE
+} from './config.js'
+
+//PERMITE LA CONEXION A VARIOS HILOS DE BD
+export const pool=createPool({
+    host:DB_HOST,
+    user:DB_USER,
+    password:DB_PASSWORD,
+    port:DB_PORT,
+    database:DB_DATABASE
 })
